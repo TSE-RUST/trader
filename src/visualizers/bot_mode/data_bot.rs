b0fc736a1ff9f3db188628c84c8bf2095b5a5776
PkCk <62013889+0PkCk0@users.noreply.github.com>
2023-01-25 21:13:07 +0100

    Data_bot allegerito

--- src/visualizers/bot_mode/data_bot.rs
+++ src/visualizers/bot_mode/data_bot.rs
@@ -10,25 +10,6 @@ use unitn_market_2022::market::Market;
 use crate::Trader;
 use crate::visualizers::datas::TraderUi;
 
-pub fn get_quantity_market(gk:GoodKind,market: Vector<f32>) -> String{
-    let mut quantity = 0.0;
-    match gk{
-        GoodKind::YEN => {
-            quantity = market[1];
-        },
-        GoodKind::USD => {
-            quantity = market[2];
-        },
-        GoodKind::YUAN => {
-            quantity = market[3];
-        },
-        GoodKind::EUR=>{
-            quantity = market[0];
-        },
-        }
-    quantity.to_string()
-}
-
 pub fn big_text(text: &str) -> impl Widget<TraderUi> {
     Label::new(text)
         .with_text_size(20.0)
@@ -36,11 +17,4 @@ pub fn big_text(text: &str) -> impl Widget<TraderUi> {
         .background(Color::rgb(255.0, 255.0, 255.0))
         .center()
 }
-
-// pub fn make_move(quantity: i32, data: &TraderUi){
-//     let mut eur_tot=data.bfb_quantities[0]+data.sol_quantities[0]+data.parse_quantities[0];
-//     if data.safe_mode{
-//         Trader::new("bot", eur_tot, , bfb, parse)
-//     }
-// }
 
