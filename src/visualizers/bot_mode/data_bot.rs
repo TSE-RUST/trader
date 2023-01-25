f366f86ecdc498269fec0128172d35fe6a3db5d5
PkCk <62013889+0PkCk0@users.noreply.github.com>
2023-01-25 17:09:20 +0100

    Data_bot

--- src/visualizers/bot_mode/data_bot.rs
+++ src/visualizers/bot_mode/data_bot.rs
@@ -1,26 +1,116 @@
-// fn get_quantity_bfb(gk:GoodKind) -> String{
-//     let mut quantity = 0.0;
-//     match gk{
-//         GoodKind::YEN => {
-//             for i in 0..data.bfb.len(){
-//                 quantity += ;
-//             }
-//         },
-//         GoodKind::USD => {
-//             for i in 0..data.sol.len(){
-//                 quantity += data.sol[i].quantity;
-//             }
-//         },
-//         GoodKind::YUAN => {
-//             for i in 0..data.parse.len(){
-//                 quantity += data.parse[i].quantity;
-//             }
-//         },
-//         GoodKind::EUR=>{
-//             for i in 0..data.bfb.len(){
-//                 quantity += data.bfb[i].quantity;
-//             }
-//         },
-//         }
-//     quantity
-// }
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use unitn_market_2022::good::good_kind::GoodKind;
+use unitn_market_2022::market::Market;
+
+fn get_quantity_bfb(gk:GoodKind,market: &Rc<RefCell<dyn Market>>) -> String{
+    let mut quantity = 0.0;
+    let market = market.borrow();
+    let goods = market.get_goods();
+    match gk{
+        GoodKind::YEN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::USD => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::YUAN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::EUR=>{
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        }
+    quantity.to_string()
+}
+
+fn get_quantity_parse(gk:GoodKind,market: &Rc<RefCell<dyn Market>>) -> String{
+    let mut quantity = 0.0;
+    let market = market.borrow();
+    let goods = market.get_goods();
+    match gk{
+        GoodKind::YEN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::USD => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::YUAN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::EUR=>{
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        }
+    quantity.to_string()
+}
+
+fn get_quantity_sol(gk:GoodKind,market: &Rc<RefCell<dyn Market>>) -> String{
+    let mut quantity = 0.0;
+    let market = market.borrow();
+    let goods = market.get_goods();
+    match gk{
+        GoodKind::YEN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::USD => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::YUAN => {
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        GoodKind::EUR=>{
+            for i in 0..goods.len() {
+                if goods[i].good_kind == gk {
+                    quantity = goods[i].quantity;
+                }
+            }
+        },
+        }
+    quantity.to_string()
+}
