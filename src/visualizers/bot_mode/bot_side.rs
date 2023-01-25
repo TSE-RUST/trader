39fde9ae33f0e661f158efd1a614578bcdc8fe37
PkCk <62013889+0PkCk0@users.noreply.github.com>
2023-01-25 21:09:14 +0100

    Bot_side new release

--- src/visualizers/bot_mode/bot_side.rs
+++ src/visualizers/bot_mode/bot_side.rs
@@ -53,99 +53,48 @@ pub(crate) fn bot_side() -> impl Widget<TraderUi>{
                 .padding(10.0)
                 .background(Color::rgb(255.0, 0.0, 0.0))
         ),
-        Container::new(
-            Split::rows(
+    Split::rows(
+        Split::rows(
+            Split::columns(
                 Split::columns(
-                        Split::columns(
-                         Split::rows(
-                                Split::rows(
-                                    Split::rows(
-                                        big_text("BFB").background(Color::rgb(255.0, 255.0, 255.0))
-                                        ,
-                                        Label::dynamic(|data: &TraderUi, _| {
-                                            format!("EUR: {}",get_quantity_market(GoodKind::EUR, data.bfb_quantities.clone()))
-                                        }).center()
-                                    ).split_point(0.5),
-                                    Split::rows(
-                                        Label::dynamic(|data: &TraderUi, _| {
-                                            format!("YEN: {}",get_quantity_market(GoodKind::YEN, data.bfb_quantities.clone()))
-                                        }).center(),
-                                        Label::dynamic(|data: &TraderUi, _| {
-                                            format!("YUAN: {}",get_quantity_market(GoodKind::YUAN, data.bfb_quantities.clone()))
-                                        }).center()
-                                    )),
-                                Label::dynamic(|data: &TraderUi, _| {
-                                    format!("USD: {}",get_quantity_market(GoodKind::YUAN, data.bfb_quantities.clone()))
-                                }).center()
-                        ).split_point(0.8)
-                        ,Split::rows(
-                            Split::rows(
-                                Split::rows(
-                                    big_text("PARSE").background(Color::rgb(255.0, 255.0, 255.0))
-                                    ,
-                                    Label::dynamic(|data: &TraderUi, _| {
-                                        format!("EUR: {}",get_quantity_market(GoodKind::EUR, data.parse_quantities.clone()))
-                                    }).center()
-                                ).split_point(0.5),
-                                Split::rows(
-                                    Label::dynamic(|data: &TraderUi, _| {
-                                        format!("YEN: {}",get_quantity_market(GoodKind::YEN, data.parse_quantities.clone()))
-                                    }).center(),
-                                    Label::dynamic(|data: &TraderUi, _| {
-                                        format!("YUAN: {}",get_quantity_market(GoodKind::YUAN, data.parse_quantities.clone()))
-                                    }).center()
-                                )),
-                            Label::dynamic(|data: &TraderUi, _| {
-                                format!("USD: {}",get_quantity_market(GoodKind::USD, data.parse_quantities.clone()))
+                        Split::rows(
+                            big_text("BFB").background(Color::rgb(255.0,255.0,255.0)),
+                            Label::dynamic(move |data: &TraderUi, _| {
+                                format!("Log BFB")
                             }).center()
-                    ).split_point(0.8)
+                        ).split_point(0.10),
+                        Split::rows(
+                            big_text("PARSE").background(Color::rgb(255.0,255.0,255.0)),
+                            Label::dynamic(move |data: &TraderUi, _| {
+                                format!("Log PARSE")
+                            }).center()
+                        ).split_point(0.10),
                     ),
                     Split::rows(
-                        Split::rows(
-                            Split::rows(
-                                big_text("SOL").background(Color::rgb(255.0, 255.0, 255.0))
-                                ,
-                                Label::dynamic(|data: &TraderUi, _| {
-                                    format!("EUR: {}",get_quantity_market(GoodKind::EUR, data.sol_quantities.clone()))
-                                }).center()
-                            ).split_point(0.5),
-                            Split::rows(
-                                Label::dynamic(|data: &TraderUi, _| {
-                                    format!("YEN: {}",get_quantity_market(GoodKind::YEN, data.sol_quantities.clone()))
-                                }).center(),
-                                Label::dynamic(|data: &TraderUi, _| {
-                                    format!("YUAN: {}",get_quantity_market(GoodKind::YUAN, data.sol_quantities.clone()))
-                                }).center()
-                            )),
-                        Label::dynamic(|data: &TraderUi, _| {
-                            format!("USD: {}",get_quantity_market(GoodKind::USD, data.sol_quantities.clone()))
+                        big_text("SOL").background(Color::rgb(255.0,255.0,255.0)),
+                        Label::dynamic(move |data: &TraderUi, _| {
+                            format!("Log SOL")
                         }).center()
-                ).split_point(0.8)
-                ).split_point(0.66).border(Color::grey(0.9), 3.0),
-                Split::rows(
-                    Split::rows(
+                    ).split_point(0.10),
+                ).split_point(0.66).border(Color::WHITE, 1.0),
                         Split::columns(
-                            Split::columns(
-                                Button::new("Fai fare 1 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
-                                    println!("1 move da fare")
+                                Split::columns(
+                                        Button::new("Fai fare 1 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
+                                            println!("1 move da fare")
+                                        }),
+                                        Button::new("Fai fare 10 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
+                                            println!("10 move da fare")
+                                        }),
+                                ),
+                                Button::new("Fai fare 30 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
+                                    println!("30 move da fare")
                                 }),
-                                Button::new("Fai fare 10 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
-                                    println!("10 move da fare")
-                                }),
-                            ),
-                            Button::new("Fai fare 30 trade al bot").on_click(|ctx, data: &mut TraderUi, _env| {
-                                println!("30 move da fare")
-                            }),
-                        ).split_point(0.66),
-                        Label::dynamic(|data: &TraderUi, _| {
-                            format!("Log del bot")
-                        }).center()
-                ).split_point(0.3),
-                    label.background(Color::rgb(255.0,227.0,0.0))
-                ).split_point(0.95)
-                )
-            )
-
+                            ).split_point(0.66),
+                        ).split_point(0.9),
+            label.background(Color::rgb(255.0,227.0,0.0))
+            ).split_point(0.95)
     ).split_point(0.07)
 
 }
+
+// label.background(Color::rgb(255.0,227.0,0.0))
